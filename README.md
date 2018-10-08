07ec08d5bbc099a94cbad138e7798d6f8958a43a
zhaoyubetter <zhaoyubetter@126.com>
2018-10-08 18:14:36 +0800

    add demo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# AndroidResourceTools
-android resouce tools
+
--- app/src/main/java/com/github/better/restools/demo/MainActivity.kt
+++ app/src/main/java/com/github/better/restools/demo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.github.better.restools.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.better_activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.better_activity_main)
+
+        btn_test.setOnClickListener {
+            text1.setText(R.string.groovy)
+            text1.setTextColor(resources.getColor(R.color.better_colorPrimary))
+            text1.setBackgroundResource(R.drawable.abc_btn_borderless_material)
+            text1.setBackgroundResource(R.drawable.alertdialog_left_selector)
+        }
     }
 }
