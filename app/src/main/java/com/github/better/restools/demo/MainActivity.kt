df2cff3fa084b662230cac7bf149a9fb718bba85
zhaoyubetter <zhaoyubetter@126.com>
2018-10-08 21:02:18 +0800

    update demo

--- app/src/main/java/com/github/better/restools/demo/MainActivity.kt
+++ app/src/main/java/com/github/better/restools/demo/MainActivity.kt
@@ -2,19 +2,19 @@ package com.github.better.restools.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.better_activity_main.*
+import kotlinx.android.synthetic.main.better_better_activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.better_activity_main)
+        setContentView(R.layout.better_better_activity_main)
 
         btn_test.setOnClickListener {
-            text1.setText(R.string.groovy)
-            text1.setTextColor(resources.getColor(R.color.better_colorPrimary))
+            text1.setText(R.string.better_groovy)
+            text1.setTextColor(resources.getColor(R.color.better_better_colorPrimary))
             text1.setBackgroundResource(R.drawable.abc_btn_borderless_material)
-            text1.setBackgroundResource(R.drawable.alertdialog_left_selector)
+            text1.setBackgroundResource(R.drawable.better_alertdialog_left_selector)
         }
     }
 }
